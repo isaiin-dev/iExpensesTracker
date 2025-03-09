73cf662a4643f56229ed9f399265c057dc1eb022
Alejandro Acosta <isaiin.dev@gmail.com>
2025-03-09 11:59:39 -0600

    feat: create initial Xcode project

---
+++ iExpensesTracker/iExpensesTrackerTests/iExpensesTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  iExpensesTrackerTests.swift
+//  iExpensesTrackerTests
+//
+//  Created by Alejandro isai Acosta Martinez on 09/03/25.
+//
+
+import Testing
+@testable import iExpensesTracker
+
+struct iExpensesTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
